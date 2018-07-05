2f1b28db1594fa0fff0e3a29120b502b09c0f9f7
maxrobot <max@grabner.co.uk>
2018-07-05 10:16:30 +0100

    Can prove a single transaction exists within a generated trie

--- proof/txRootExample_test.go
+++ proof/txRootExample_test.go
@@ -2,174 +2,117 @@ package main
 
 import (
 	"bytes"
-	crand "crypto/rand"
+	"context"
+	"log"
+	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/maxrobot/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/maxrobot/go-ethereum/trie"
+	"github.com/stretchr/testify/assert"
 )
-
-type kv struct {
-	k, v []byte
-	t    bool
-}
 
 var expectedBlockHash = common.HexToHash("0xa68e48252380b056f5e1c1a738897b84148be4ffbcf0857effbaa086a8a99fbb")
 var expectedTx = common.HexToHash("0xd828cadfcc7694d314058404506fc10a4dadac72aba68c286b34137da4156630")
 
-// makeProvers creates Merkle trie provers based on different implementations to
-// test all variations.
-func makeProvers(trieObj *trie.Trie) []func(key []byte) *ethdb.MemDatabase {
-	var provers []func(key []byte) *ethdb.MemDatabase
-
-	// Create a direct trie based Merkle prover
-	provers = append(provers, func(key []byte) *ethdb.MemDatabase {
-		proof := ethdb.NewMemDatabase()
-		trieObj.Prove(key, 0, proof)
-		return proof
-	})
-	// Create a leaf iterator based Merkle prover
-	provers = append(provers, func(key []byte) *ethdb.MemDatabase {
-		proof := ethdb.NewMemDatabase()
-		if it := trie.NewIterator(trieObj.NodeIterator(key)); it.Next() && bytes.Equal(key, it.Key) {
-			for _, p := range it.Prove() {
-				proof.Put(crypto.Keccak256(p), p)
-			}
-		}
-		return proof
-	})
-	return provers
+func updateString(trie *trie.Trie, k, v string) {
+	trie.Update([]byte(k), []byte(v))
 }
 
-func randomTrie(n int) (*trie.Trie, map[string]*kv) {
+func TestOneElementProof(t *testing.T) {
 	trieObj := new(trie.Trie)
-	vals := make(map[string]*kv)
-	for i := byte(0); i < 100; i++ {
-		value := &kv{common.LeftPadBytes([]byte{i}, 32), []byte{i}, false}
-		value2 := &kv{common.LeftPadBytes([]byte{i + 10}, 32), []byte{i}, false}
-		trieObj.Update(value.k, value.v)
-		trieObj.Update(value2.k, value2.v)
-		vals[string(value.k)] = value
-		vals[string(value2.k)] = value2
+	var idx = "spoon"
+	var leaf = "kita"
+
+	// Put some stuff in the trie
+	updateString(trieObj, "niki", "fmg")
+	updateString(trieObj, "spoon", "kita")
+	updateString(trieObj, "spoon2", "kita2")
+	updateString(trieObj, "spoon3", "kita3")
+	updateString(trieObj, "spoon4", "kita4")
+
+	// Generate a proof
+	proof := ethdb.NewMemDatabase()
+	trieObj.Prove([]byte(idx), 0, proof)
+	if proof == nil {
+		t.Fatalf("prover: nil proof")
 	}
-	for i := 0; i < n; i++ {
-		value := &kv{randBytes(32), randBytes(20), false}
-		trieObj.Update(value.k, value.v)
-		vals[string(value.k)] = value
+
+	// Verify the proof
+	val, _, err := trie.VerifyProof(trieObj.Hash(), []byte(idx), proof)
+	if err != nil {
+		t.Fatalf("prover: failed to verify proof: %v\nraw proof: %x", err, proof)
 	}
-	return trieObj, vals
+	if !bytes.Equal(val, []byte(leaf)) {
+		t.Fatalf("prover: verified value mismatch: have %x, want 'k'", val)
+	}
+
 }
 
-func randBytes(n int) []byte {
-	r := make([]byte, n)
-	crand.Read(r)
-	return r
-}
+func TestOneTransactionProof(t *testing.T) {
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func TestProofMod(t *testing.T) {
-	trieObj, vals := randomTrie(500)
-	root := trieObj.Hash()
-	for i, prover := range makeProvers(trieObj) {
-		for _, kv := range vals {
-			proof := prover(kv.k)
-			if proof == nil {
-				t.Fatalf("prover %d: missing key %x while constructing proof", i, kv.k)
-			}
-			val, _, err := trie.VerifyProof(root, kv.k, proof)
-			if err != nil {
-				t.Fatalf("prover %d: failed to verify proof for key %x: %v\nraw proof: %x", i, kv.k, err, proof)
-			}
-			if !bytes.Equal(val, kv.v) {
-				t.Fatalf("prover %d: verified value mismatch for key %x: have %x, want %x", i, kv.k, val, kv.v)
-			}
+	// Select a specific block
+	num := "5904064"
+	blockNumber := new(big.Int)
+	blockNumber.SetString(num, 10)
+
+	// Fetch header of block num
+	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, expectedBlockHash, header.Hash())
+
+	// Fetch block of block num
+	block, err := client.BlockByNumber(context.Background(), header.Number)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, expectedBlockHash, block.Hash())
+
+	// Select a transaction, index should be 49
+	transx := block.Transaction(expectedTx)
+	var txIdx []byte
+	var leaf []byte
+	// fmt.Printf("\nTransaction:\n% 0x", transx.Hash().Bytes())
+
+	// Generate the trie
+	trieObj := new(trie.Trie)
+	for idx, tx := range block.Transactions() {
+		rlpIdx, _ := rlp.EncodeToBytes(uint(idx))  // rlp encode index of transaction
+		rlpTransaction, _ := rlp.EncodeToBytes(tx) // rlp encode transaction
+
+		trieObj.Update(rlpIdx, rlpTransaction)
+		// fmt.Printf("%x\t%x\n", transx.Hash().Bytes(), tx.Hash().Bytes())
+		if transx == tx {
+			txIdx = rlpIdx
+			leaf = rlpTransaction
 		}
+
 	}
+
+	// Generate a proof
+	proof := ethdb.NewMemDatabase()
+	trieObj.Prove(txIdx, 0, proof)
+	if proof == nil {
+		t.Fatalf("prover: nil proof")
+	}
+
+	// Verify the proof
+	val, _, err := trie.VerifyProof(trieObj.Hash(), txIdx, proof)
+	if err != nil {
+		t.Fatalf("prover: failed to verify proof: %v\nraw proof: %x", err, proof)
+	}
+	if !bytes.Equal(val, leaf) {
+		t.Fatalf("prover: verified value mismatch: have %x, want 'k'", val)
+	}
+	// fmt.Printf("\nVerified Value:\t%x\nExpected Leaf:\t%x\n", val, leaf)
+
 }
-
-// func TestProof(t *testing.T) {
-// 	trieObj, vals := randomTrie(500)
-// 	root := trieObj.Hash()
-// 	for i, prover := range makeProvers(trieObj) {
-// 		for _, kv := range vals {
-// 			proof := prover(kv.k)
-// 			if proof == nil {
-// 				t.Fatalf("prover %d: missing key %x while constructing proof", i, kv.k)
-// 			}
-// 			val, _, err := trie.VerifyProof(root, kv.k, proof)
-// 			if err != nil {
-// 				t.Fatalf("prover %d: failed to verify proof for key %x: %v\nraw proof: %x", i, kv.k, err, proof)
-// 			}
-// 			if !bytes.Equal(val, kv.v) {
-// 				t.Fatalf("prover %d: verified value mismatch for key %x: have %x, want %x", i, kv.k, val, kv.v)
-// 			}
-// 		}
-// 	}
-// }
-
-// func Test_txRootExample(t *testing.T) {
-// 	client, err := ethclient.Dial("https://mainnet.infura.io")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Println("we have a connection")
-
-// 	// Select a specific block
-// 	num := "5904064"
-// 	blockNumber := new(big.Int)
-// 	blockNumber.SetString(num, 10)
-
-// 	// Fetch header of block num
-// 	header, err := client.HeaderByNumber(context.Background(), blockNumber)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	assert.Equal(t, expectedBlockHash, header.Hash())
-
-// 	// Fetch block of block num
-// 	block, err := client.BlockByNumber(context.Background(), header.Number)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	assert.Equal(t, expectedBlockHash, block.Hash())
-
-// 	// Select a transaction, index should be 49
-// 	transx := block.Transaction(expectedTx)
-// 	var txIdx []byte
-// 	fmt.Printf("\nTransaction:\n% 0x", transx.Hash().Bytes())
-
-// 	// Generate the trie
-// 	trieObj := new(trie.Trie)
-// 	for idx, tx := range block.Transactions() {
-// 		rlpIdx, _ := rlp.EncodeToBytes(uint(idx))  // rlp encode index of transaction
-// 		rlpTransaction, _ := rlp.EncodeToBytes(tx) // rlp encode transaction
-
-// 		trieObj.Update(rlpIdx, rlpTransaction)
-// 		fmt.Printf("%x\t%x\n", transx.Hash().Bytes(), tx.Hash().Bytes())
-// 		if transx == tx {
-// 			fmt.Println(rlpIdx)
-// 			txIdx = rlpIdx
-// 		}
-
-// 	}
-
-// 	// Root hash
-// 	root := trieObj.Hash()
-// 	fmt.Printf("\nTransaction:\n% 0x\n", root)
-
-// 	// Create the proof for transaction
-// 	key, _ := rlp.EncodeToBytes(transx)
-// 	proof := ethdb.NewMemDatabase()
-// 	trieObj.Prove(key, 0, proof)
-// 	if proof == nil {
-// 		fmt.Println("Error no proof produced!")
-// 	}
-
-// 	fmt.Println(txIdx)
-// 	// Verify the proof exists
-// 	val, _, err := trie.VerifyProof(root, txIdx, proof)
-// 	fmt.Println(val)
-
-// }
